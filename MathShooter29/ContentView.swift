debd8eedcd29ce67881432382ce33b889c80716f
Varanpong Suthiponpisal <porchportal4@gmail.com>
2023-12-19 22:17:50 +0700

    Initial Commit

---
+++ MathShooter29/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathShooter29
+//
+//  Created by วรัญพงษ์ สุทธิพนไพศาล on 19/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathShooter29/MathShooter29App.swift
@@ -0,0 +1,17 @@
+//
+//  MathShooter29App.swift
+//  MathShooter29
+//
+//  Created by วรัญพงษ์ สุทธิพนไพศาล on 19/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MathShooter29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
